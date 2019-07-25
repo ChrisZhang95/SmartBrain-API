86400724db085b76fb43e324233f468d3b8719de
ChrisZhang95 <chris.zhang95@hotmail.com>
2019-07-25 19:51:59 -0400

    changed port number

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => {image.handleApiCall  (req, res)})
 
 
-app.listen(preocess.env.PORT || 3000, () => {
-  console.log(`app is runnning on port ${preocess.env.PORT}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is runnning on port ${process.env.PORT}`);
 });
